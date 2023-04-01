351bd5eebc791e5c28abbabac9c1cc567f0bcd2f
IselaMartin1408 <isela.martin@hotmail.com>
2023-04-01 11:49:33 -0600

    MatrizAleatoria

--- matrix_operations.c
+++ matrix_operations.c
@@ -3,13 +3,28 @@
 #include <math.h>
 #include <omp.h>
 
-int main(){
- 
- int num_threads = omp_get_max_threads();
-    printf("Using %d threads\n", num_threads);
+void Usage(char* prog_name);
+double **generate_matrix(int n);
 
-    double A[2][2] = {{1.0, 2.0},
-                      {3.0, 4.0}};
+int main(int argc, char* argv[]){
+    int  n, num_threads;
+
+    if (argc != 2) Usage(argv[0]);
+    n = strtoll(argv[1], NULL, 10);
+
+    double **A = generate_matrix(n);
+    
+    printf("Matriz generada aleatoriamente:\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%.3f  ", A[i][j]);
+        }
+        printf("\n");
+    }
+
+    num_threads = omp_get_max_threads();
+    printf("Using %d threads\n", num_threads);
+                      
     double eigenvalues[2];
     double eigenvectors[2][2];
     #pragma omp parallel sections shared(A, eigenvalues, eigenvectors)
@@ -18,7 +33,29 @@ int main(){
         {
 			double discriminant = sqrt(pow(A[0][0] + A[1][1], 2) - 4 * (A[0][0] * A[1][1] - A[0][1] * A[1][0]));
         }
-
+    }
     return 0;
 }
+
+//la matriz debe ser cuadrada por lo que solo se pide un valor para generar la matriz
+void Usage(char* prog_name) {
+   fprintf(stderr, "Usage: %s <n>\n", prog_name);
+   fprintf(stderr, "   n: number of rows and columns in matrix\n");
+}
+
+double **generate_matrix(int n) {
+    double **A = (double **) malloc(n * sizeof(double *));
+    #pragma omp parallel for
+    for (int i = 0; i < n; i++) {
+        A[i] = (double *) malloc(n * sizeof(double));
+        #pragma omp parallel for
+        for (int j = 0; j < n; j++) {
+            A[i][j] = ((double) rand() / (double) RAND_MAX) * 1000;
+        }
+    }
+    return A;
+}
+
+
+
 
