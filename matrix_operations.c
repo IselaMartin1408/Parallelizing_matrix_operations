558913bacbdbb0ddada9146dff98f7654eb94fcb
IselaMartin1408 <isela.martin@hotmail.com>
2023-04-01 11:58:23 -0600

    MatrizAleatoriaV2

--- matrix_operations.c
+++ matrix_operations.c
@@ -37,19 +37,21 @@ int main(int argc, char* argv[]){
     return 0;
 }
 
-//la matriz debe ser cuadrada por lo que solo se pide un valor para generar la matriz
+//la matriz debe ser cuadrada para eigenvalores e inversa por lo que solo se pide un valor para generar la matriz
 void Usage(char* prog_name) {
    fprintf(stderr, "Usage: %s <n>\n", prog_name);
    fprintf(stderr, "   n: number of rows and columns in matrix\n");
 }
 
+
 double **generate_matrix(int n) {
+    int i,j;
     double **A = (double **) malloc(n * sizeof(double *));
-    #pragma omp parallel for
-    for (int i = 0; i < n; i++) {
+    #pragma omp parallel for private(i,j) shared(A)
+    for ( i = 0; i < n; i++) {
         A[i] = (double *) malloc(n * sizeof(double));
         #pragma omp parallel for
-        for (int j = 0; j < n; j++) {
+        for ( j = 0; j < n; j++) {
             A[i][j] = ((double) rand() / (double) RAND_MAX) * 1000;
         }
     }
