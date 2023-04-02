cdcc3e385e89affcc1419ef0a587837991cd11a3
pablog179 <pmarmol179@gmail.com>
2023-04-02 03:48:21 -0600

    matrizin

--- matrix_operations.c
+++ matrix_operations.c
@@ -5,6 +5,7 @@
 
 void Usage(char* prog_name);
 double **generate_matrix(int n);
+double **matrix_inverse(double **A, int n);
 
 int main(int argc, char* argv[]){
     int  n, num_threads;
@@ -14,10 +15,21 @@ int main(int argc, char* argv[]){
 
     double **A = generate_matrix(n);
     
+    
     printf("Matriz generada aleatoriamente:\n");
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             printf("%.3f  ", A[i][j]);
+        }
+        printf("\n");
+    }
+
+    double **A_inv = matrix_inverse(A, n);
+    
+    printf("Matriz inversa:\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%.3f  ", A_inv[i][j]);
         }
         printf("\n");
     }
@@ -58,6 +70,43 @@ double **generate_matrix(int n) {
     return A;
 }
 
+double **matrix_inverse(double **A, int n) {
+    double **A_inv = (double **) malloc(n * sizeof(double *));
+    #pragma omp parallel for
+    for (int i = 0; i < n; i++) {
+        A_inv[i] = (double *) malloc(n * sizeof(double));
+    }
 
+    double det = 0;
+    double **adj = (double **) malloc(n * sizeof (double *));
+    #pragma omp parallel for
+    for (int i = 0; i < n; i++) {
+        adj[i] = (double *) malloc(n * sizeof(double));
+    }
 
+    // Determinante
+    for (int i = 0; i < n; i++) {
+        det = det + (A[0][i] * (A[1][(i+1)%n] * A[2][(i+2)%n] - A[1][(i+2)%n] * A[2][(i+1)%n]));
+    }
 
+    if (det == 0) {
+        printf("La matriz no es invertible.\n");
+        return A_inv;
+    }
+
+    // Adjunta
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            adj[i][j] = pow(-1, i+j) * (A[(j+1)%n][(i+1)%n] * A[(j+2)%n][(i+2)%n] - A[(j+1)%n][(i+2)%n] * A[(j+2)%n][(i+1)%n]);
+        }
+    }
+
+    // Matriz inversa
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            A_inv[i][j] = adj[i][j] / det;
+        }
+    }
+
+    return A_inv;
+}
