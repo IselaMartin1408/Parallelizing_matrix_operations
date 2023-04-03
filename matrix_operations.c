c56f1b1e4fd19804018b0e7744ba227997a19cd8
joshuaGMO <joshua725gmo@gmail.com>
2023-04-03 16:04:58 -0600

    cambio

--- matrix_operations.c
+++ matrix_operations.c
@@ -260,8 +260,26 @@ void transpose(double **adj, int n) {
 //-----------------------------------------------------------------
 void mxm(double **A,double **C, int n)
 {
+    int j,k;
     double MT[n][n];
+     // Inicializar la matriz C a cero
+    #pragma omp parallel for private(j,k)
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            MT[i][j] = 0;
+        }
+    }
 
+    // Multiplicar las matrices A y B
+    #pragma omp parallel for private(j,k)
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            for (int k = 0; k < n; k++) {
+                MT[i][j] += A[i][k] * C[k][j];
+            }
+        }
+    }
 
+Print_matrix(MT,n, "MULTIPLICACION mXm");
     
 }
